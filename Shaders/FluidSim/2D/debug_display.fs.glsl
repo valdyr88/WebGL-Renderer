87f5dcc343f2e0ce4b651f969dc9e33fdd89485f
Mihajlo Arbanas <valdyr88@gmail.com>
2018-12-10 19:07:15 +0000

    git-svn-id: https://r7/svn/WebGL@31 45b56d4a-f8b8-b242-b192-c14f84797ed6

---
+++ Shaders/FluidSim/2D/debug_display.fs.glsl
@@ -0,0 +1,89 @@
+#version 300 es
+// #extension GL_EXT_shader_texture_lod : require
+precision mediump float;
+//display raznih fluidsim vrijednosti
+
+#global_defines
+#include "defines"
+#include "functions"
+//------------------------------------------------------------------------------
+
+#if __VERSION__ >= 300
+	#define gl_FragColor out_FragColor
+	layout(location = 0) out vec4 out_FragColor;
+	// layout(location = 1) out vec4 out_Normal;
+	// layout(location = 2) out vec4 out_AoRSMt;
+#endif
+
+#if __VERSION__ >= 120
+	#define texture2D texture
+	#define textureCube texture
+	#define textureCubeLod textureLod
+#endif
+
+uniform vec2 aspect; //odnos dimenzija teksture i svijeta
+uniform float dT;
+uniform float k; //kinematic viscosity, = viscosity / density
+
+// uniform sampler2D txAdvectedVelocity;
+uniform sampler2D txVelocity;
+uniform sampler2D txPressure;
+uniform sampler2D txVelocityDivergence;
+//------------------------------------------------------------------------------
+#define varyin in
+
+varyin vec2 TexCoords;
+//------------------------------------------------------------------------------
+
+vec2 toTexSpace(vec2 x){ return x*aspect; }
+vec2 toWorldSpace(vec2 x){ return x/aspect; }
+
+vec4 samplePoint(sampler2D tx, vec2 x){
+	ivec2 txSize = textureSize(tx, 0);
+	return texelFetch(tx, ivec2(toTexSpace(x)*txSize)); //sample point
+}
+vec4 samplePoint(sampler2D tx, ivec2 txSize, vec2 x){
+	return texelFetch(tx, ivec2(toTexSpace(x)*txSize));
+}
+vec4 sampleLinear(sampler2D tx, vec2 x){
+	return texture2D(tx, toTexSpace(x));
+}
+
+/* 
+	#define _DEBUG_Display_Velocity
+	#define _DEBUG_Display_Pressure
+	#define _DEBUG_Display_Divergence
+*/
+
+void main(void)
+{	
+	ivec2 size = textureSize(txVelocity, 0);
+	vec2 x = toWorldSpace(TexCoords);
+	const float dt = dT;	
+	const vec2 dx = vec2(1.0,1.0);
+	
+	vec4 rtn = vec4(0.0,0.0,0.0,1.0);
+	
+	#if defined(_DEBUG_Display_Velocity)	
+		vec4 v = texture2D(txVelocity, TexCoords);
+		rtn.xyz = v.xyz*0.5f + 0.5f;
+		
+	#elif defined(_DEBUG_Display_Pressure)
+		float p = texture2D(txPressure, TexCoords).x;
+		rtn.xyz = p;
+		
+	#elif defined(_DEBUG_Display_Divergence)
+		float div = texture2D(txVelocityDivergence, TexCoords).x;
+		rtn.xyz = div;
+		
+	#endif
+	
+	out_FragColor = rtn;
+}
+
+
+
+
+
+
+
