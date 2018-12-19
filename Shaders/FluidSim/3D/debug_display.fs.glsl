d86b0e7b4cbe638cf21c390537dfed6af898a97f
Mihajlo Arbanas <valdyr88@gmail.com>
2018-12-19 21:53:49 +0000

    git-svn-id: https://r7/svn/WebGL@87 45b56d4a-f8b8-b242-b192-c14f84797ed6

--- Shaders/FluidSim/3D/debug_display.fs.glsl
+++ Shaders/FluidSim/3D/debug_display.fs.glsl
@@ -50,12 +50,12 @@ varyin vec2 TexCoords;
 	#define _DEBUG_Display_Divergence
 */
 
-void main(void)
+void main_OneLayer(void)
 {	
-	// int z = int((cos(Time)*0.5+0.5)*Resolution.z);
+	// int z = int((cos(Time)*0.25+0.5)*Resolution.z);
 	int z = int(0.5*Resolution.z);
 	vec3 x = toTexSpace(toWorldSpace(TexCoords, z));
-	float dt = dT;	
+	float dt = dT;
 	const vec3 dx = vec3(1.0,1.0,1.0);
 	
 	vec4 rtn = vec4(0.0,0.0,0.0,1.0);
@@ -90,5 +90,7 @@ void main(void)
 
 
 
-
+void main(void){
+	main_OneLayer();
+}
 
